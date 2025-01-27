a89de5e9d08842ea3e9101bf6d417522c08c7941
Leandro de Souza Cunha <leandro.kate@gmail.com>
2025-01-27 17:26:15 -0300

    Validar a aula novamente do JS

---
+++ js/predio.js
@@ -0,0 +1,44 @@
+(function () {
+    // Pavimentos 
+    function criarTerreo() {
+        const janela = document.createElement('div')
+        janela.classList.add('janela')
+
+        const terreo = document.createElement('div')
+        terreo.classList.add('terreo')
+
+        terreo.appendChild(janela)
+
+        return terreo
+    }
+
+    function criarAndar() {
+        const porta = document.createElement('div')
+        porta.classList.add('porta')
+
+        const andar = document.createElement('div')
+        andar.classList.add('andar')
+        andar.appendChild(porta)
+        
+        return andar 
+    }
+
+    function criarPavimentos() {
+        const elementosComAndares = document.querySelectorAll('[andares]')
+        elementosComAndares.forEach(elComAndares => {
+        const qtde = +elComAndares.getAttribute('andares')
+
+        for(let i = 0; i < qtde; i++) {
+            elComAndares.appendChild(criarAndar())
+        }
+
+        elComAndares.appendChild(criarTerreo())
+        })
+    }
+
+    criarPavimentos()
+
+    
+
+    // Elevador 
+})
